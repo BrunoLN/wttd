ca61f98a8a8c0c8b18e00a54759ace68de8e3979
Bruno Nascimento <brunolnascimento@gmail.com>
2013-07-29 01:11:49 -0300

    Refatoração na app subscriptions, criação do pacote tests e separação dos testes.

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,4 +1,10 @@
 # coding: utf-8
+from django import forms
+from django.utils.translation import ugettext as _
 
-class SubscriptionForm(object):
-    pass
+class SubscriptionForm(forms.Form):
+    name = forms.CharField(label=_('Nome'))
+    cpf = forms.CharField(label=_('CPF'))
+    email = forms.CharField(label=_('Email'))
+    phone = forms.CharField(label=_('Telefone'))
+
